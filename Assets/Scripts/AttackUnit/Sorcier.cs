823a48dfe4ca7184eff9a019cd32ec0597ed0f78
ValT <v.lantigny@gmail.com>
2023-12-19 14:45:09 +0100

    Ajout du tank

---
+++ Assets/Scripts/AttackUnit/Sorcier.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Sorcier : AttackController
+{
+    public override void StartBehavior()
+    {
+        
+    }
+
+    public override void UpdateBehavior()
+    {
+        
+    }
+
+}
